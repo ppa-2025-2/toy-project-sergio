cad77e0128f3bf4fba5c0cd6d4d1d0a6a65691af
sergiolmendonca <sergiopromail@gmail.com>
2025-11-26 06:50:48 -0300

    remoção de arquivos desnecessários

--- ms-user/src/main/java/com/example/demo/Controller/DTO/AcceptTicketDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Controller.DTO;
-
-import com.example.demo.Repository.Entity.Ticket.Status;
-
-public record AcceptTicketDTO(
-        Status novoStatus,
-        Integer responsavel
-) {}
